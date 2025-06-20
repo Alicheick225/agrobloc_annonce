3ab4f9dfd96dd9abe2e0f50146237f74dfe63216
ashley612 <ashleygawa612@gmail.com>
2025-06-20 16:49:42 +0000

    Deleting of from django.py

--- annonces/from django.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import AnnonceVente
-
-@admin.register(AnnonceVente)
-class AnnonceVenteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'type_culture_id', 'parcelle_id', 'prevision', 'statut', 'quantite', 'prix_kg')
